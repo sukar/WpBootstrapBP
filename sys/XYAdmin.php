db6f49229dbfa0c580d339c34ed944a696c8d785
Sukar YM <sukar@lab1521.com>
2012-04-17 23:34:19 +1200

    Add blank options page for the meantime

--- sys/XYAdmin.php
+++ sys/XYAdmin.php
@@ -7,16 +7,18 @@
 class XYAdmin extends XYHelper {
 
     var $xytKey = "_XYThemes_Key",
-            $xytLnk = "_XYThemes_Lnk",
-            $xytJson = "_XYThemes_Jsn",
-            $xytNow = "";
+        $xytLnk = "_XYThemes_Lnk",
+        $xytJson = "_XYThemes_Jsn",
+        $xytNow = "";
 
-    function __construct() {
+    function __construct() 
+    {
         parent::__construct();
         $this->registerapi();
     }
 
-    function registerapi() {
+    function registerapi() 
+    {
         register_sidebar(array(
             'name' => __('Left Sidebar', 'xythemes'),
             'id' => 'sidebar-1',
@@ -43,17 +45,30 @@ class XYAdmin extends XYHelper {
         );
 
         add_filter('wp_page_menu_args', array(&$this, 'main_menu_args'));
+
+        // Load the Admin Options page
+        add_action('admin_menu', array(&$this, 'menu_options'));
+
+        // Settings API options initilization and validation
+        add_action( 'admin_init', array(&$this, 'register_options'));
+
+    }
+
+    function register_options(){
+      // Register Settings
+      register_setting( 'xythemes_options', 'xythemes_options', array(&$this, 'options_validate'));
     }
     
-        /*
-     * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
-     */
-    function main_menu_args( $args ) {
+    
+    // Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
+    function main_menu_args( $args ) 
+    {
       $args['show_home'] = true;
       return $args;
     }
     
-    function register_post_gallery() {
+    function register_post_gallery() 
+    {
       register_post_type( 'xythemes_gallery',
           array(
               'labels' => array(
@@ -65,8 +80,63 @@ class XYAdmin extends XYHelper {
               'rewrite' => array('slug' => 'gallery')
           )
       );
-    } 
+    }
+
+    function menu_options() 
+    {
+      add_theme_page('XYTHemes Options', 'XYThemes', 'edit_theme_options', 'xythemes-settings', array(&$this, 'admin_options_page'));
+    }
+
+    function get_settings_page_tabs() 
+    {
+      $tabs = array(
+          'general' => 'General',
+          'personalize' => 'Personalize'
+     );
+     return $tabs;
+    }
+
+    // Admin settings page markup
+    function admin_options_page() 
+    { ?>
+         <div class="wrap">
+              <?php $this->admin_options_page_tabs(); ?>
+              <?php if ( isset( $_GET['settings-updated'] ) ) {
+                   echo '<div class="updated"><p>Theme settings updated successfully.</p></div>';
+              } ?>
+         <form action="options.php" method="post">
+         <?php
+         settings_fields('xythemes_options');
+         do_settings_sections('xythemes');
+         ?>
+         <?php $tab = ( isset( $_GET['tab'] ) ? $_GET['tab'] : 'general' ); ?>
+         <input name="xythemes_options[submit-<?php echo $tab; ?>]" type="submit" class="button-primary" value="<?php esc_attr_e('Save Settings', 'xythemes'); ?>" />
+         <input name="xythemes_options[reset-<?php echo $tab; ?>]" type="submit" class="button-secondary" value="<?php esc_attr_e('Reset Defaults', 'xythemes'); ?>" />
+         </form>
+         </div>
+    <?php }
+
+    function admin_options_page_tabs( $current = 'general' ) 
+    {
+      if ( isset ( $_GET['tab'] ) ) :
+          $current = $_GET['tab'];
+      else:
+          $current = 'general';
+      endif;
+      $tabs = $this->get_settings_page_tabs();
+      $links = array();
+      foreach( $tabs as $tab => $name ) :
+          if ( $tab == $current ) :
+               $links[] = '<a class="nav-tab nav-tab-active" href="?page=xythemes-settings&tab='.$tab.'">'.$name.'</a>';
+          else :
+               $links[] = '<a class="nav-tab" href="?page=xythemes-settings&tab='.$tab.'">'.$name.'</a>';
+          endif;
+      endforeach;
+      echo '<div id="icon-themes" class="icon32"><br /></div>';
+      echo '<h2 class="nav-tab-wrapper">';
+      foreach ( $links as $link )
+          echo $link;
+      echo '</h2>';
+    }
 
 }
-
-?>
